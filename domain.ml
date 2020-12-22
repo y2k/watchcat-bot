d42a0d55e9d4d791fd0211eb4842baf7bf8a6b10
y2k <itwisterlx@gmail.com>
2020-12-22 20:06:42 +0300

    refactoring

---
+++ domain.ml
@@ -0,0 +1,44 @@
+module IntMap = Map.Make (Int)
+module IntSet = Set.Make (Int)
+
+type state = {trusted_users: IntSet.t; user_registerd: float IntMap.t}
+
+let empty_state = {trusted_users= IntSet.empty; user_registerd= IntMap.empty}
+
+let try_ban env user_id bar_user_id =
+  let state = env#state in
+  if IntSet.mem user_id state.trusted_users then
+    match IntMap.find_opt bar_user_id state.user_registerd with
+    | Some reg_date ->
+        let reg_duration = env#now -. reg_date in
+        if reg_duration < 2.0 *. 24.0 *. 3600.0 then `KickUser bar_user_id
+        else
+          `SendMessage
+            (Printf.sprintf
+               "Пользователь добавился слишком \
+                давно (%f сек)."
+               reg_duration)
+    | _ ->
+        `SendMessage
+          "Пользователь добавился слишком \
+           давно или ненайден."
+  else
+    `SendMessage
+      "Извините, но вас нет в списке \
+       доверенных пользователей. Обратитесь \
+       пожалуйста к админам, для добавления в \
+       список."
+
+let add_trusted_user env user_id =
+  match user_id with
+  | Some user_id ->
+      let state = env#state in
+      `UpdateState
+        {state with trusted_users= IntSet.add user_id state.trusted_users}
+  | None ->
+      `SendMessage "Пользователь не указан"
+
+let new_chat_member env user_id =
+  let state = env#state in
+  `UpdateState
+    {state with user_registerd= IntMap.add user_id env#now state.user_registerd}
--- main.ml
+++ main.ml
@@ -1,66 +1,79 @@
-module IntMap = Map.Make (Int)
-
-module Domain = struct
-  let trusted_users = [241854720]
-
-  let user_registerd : int IntMap.t = IntMap.empty
-
-  let contains xs x =
-    match List.find_opt (fun i -> x = i) xs with
-    | Some _ ->
-        true
-    | None ->
-        false
-
-  let try_ban userId banUserId =
-    if contains trusted_users userId then
-      let regDate = IntMap.find_opt banUserId user_registerd in
-      match regDate with Some _ -> `KickUser banUserId | None -> `None
-    else `None
-end
-
+open Domain
 open Telegram.Api
 
 module WatchcatBot = Mk (struct
-  open Chat
   open Command
   open Message
   include Telegram.BotDefaults
 
   let token = Sys.getenv "TELEGRAM_TOKEN"
 
-  let command_postfix = Some "watchman"
+  let command_postfix = Some "watchcat"
 
-  let handleEffect chatId userId e =
+  let state_store = ref empty_state
+
+  let handleEffect chat_id effect =
     let open Telegram.Actions in
-    match e with
+    match effect with
     | `KickUser user ->
-        kick_chat_member ~chat_id:chatId ~user_id:user
+        kick_chat_member ~chat_id ~user_id:user
+    | `UpdateState state ->
+        state_store := state ;
+        nothing
+    | `SendMessage message ->
+        send_message ~chat_id "%s" message
     | `None ->
-        send_message ~chat_id:chatId "UserID: %i" userId
+        nothing
+
+  let make_env () =
+    object
+      method state = !state_store
+
+      method now = Unix.time ()
+    end
+
+  let new_chat_member (chat : Chat.chat) (user : User.user) =
+    Domain.new_chat_member (make_env ()) user.id |> handleEffect chat.id
+
+  let find_user_in_message entities =
+    let open MessageEntity in
+    entities
+    |> Option.fold ~none:[] ~some:(fun x -> x)
+    |> List.find_opt (fun x ->
+           match x.entity_type with TextMention _ -> true | _ -> false)
+    |> function
+    | Some {entity_type= TextMention user; _} -> Some user.id | _ -> None
 
   let commands =
-    let try_ban {chat= {id= chatId; _}; from; reply_to_message; _} =
-      match (from, reply_to_message) with
-      | Some {id= userId; _}, Some {from= Some {id= repl_user_id; _}; _} ->
-          Domain.try_ban userId repl_user_id |> handleEffect chatId userId
+    let try_ban = function
+      | { chat= {id= chatId; _}
+        ; from= Some {id= userId; _}
+        ; reply_to_message= Some {from= Some {id= repl_user_id; _}; _}
+        ; _ } ->
+          Domain.try_ban (make_env ()) userId repl_user_id
+          |> handleEffect chatId
       | _ ->
           failwith "???"
+    and add_trusted_user = function
+      | {chat= {id= chatId; _}; entities; _} ->
+          Domain.add_trusted_user (make_env ()) (find_user_in_message entities)
+          |> handleEffect chatId
     in
-    [{name= "ban"; description= "Ban user"; enabled= true; run= try_ban}]
+    [ {name= "ban"; description= "Ban user"; enabled= true; run= try_ban}
+    ; { name= "add_tu"
+      ; description= "Add trusted user"
+      ; enabled= true
+      ; run= with_auth ~command:add_trusted_user } ]
 end)
 
 open Lwt
 
-let run ?(log = true) () =
+let run () =
   let process = function
     | Result.Success _ ->
         return ()
     | Result.Failure e ->
-        if log && e <> "Could not get head" then
-          (* Ignore spam *)
-          Lwt_io.printl e
-        else return ()
+        if e <> "Could not get head" then Lwt_io.printl e else return ()
   in
   let rec loop () =
     WatchcatBot.pop_update ~run_cmds:true () >>= process >>= loop
